da7a189812bce00301ae5d5815b16aa66abe25d6
Paul McManus <pmcmanus2021@gmail.com>
2020-06-15 13:16:37 +0100

    Fetches username's data, set's collection to state with hooks, will merge axios into master.

--- src/containers/CollectionContainer.js
+++ src/containers/CollectionContainer.js
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
-
-
 const CollectionContainer = ({ match }) => {
 
     const [collection, setCollection] = useState([]);
+
     const { params: { id } } = match;
     const username = match.params.id
+    const fetchURL = "https://bgg-json.azurewebsites.net/collection/" + username
+
+    const fetchData = () => {
+        axios.get(`${fetchURL}`).then(res => {
+            console.log(res.data); 
+            setCollection(res.data)        
+        });
+    };
     
     useEffect(() => {
-        axios.get("https://bgg-json.azurewebsites.net/collection/" + username)
-            .then(res => {
-            const data = res.data;
-            console.log(data);
-      });   
-    })
-    
+        fetchData();
+      },[]);
 
-    
-
-    return(
+    return (
         <div className="collection-container">
            <h1>{username}'s collection</h1>
            <button onClick={(e) => window.location="/"}>Home</button>
         </div>
-    )
+    );
 }
 
 export default CollectionContainer;
