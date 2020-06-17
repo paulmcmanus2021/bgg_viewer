d22f3a32abbf93227aa27954abc20cb6cadb8a3b
Paul McManus <pmcmanus2021@gmail.com>
2020-06-17 15:30:09 +0100

    Before installing r-bootstrap

--- src/containers/ErrorContainer.js
+++ src/containers/ErrorContainer.js
@@ -8,8 +8,8 @@ function ErrorContainer(props){
 
     return(
         <div className="error-container">
-            <h1>Error</h1>
-            <h2>{username} does not exist, check spelling and try again</h2>
+            <h1>Sorry!</h1>
+            <h2>{username} does not exist, check spelling and try again.</h2>
             <Input />
         </div>
     )
